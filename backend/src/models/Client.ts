cd25eca8a3e3bb9b4feaf185f7efc3e04594a337
Adrian Azor <Adrianazor0@hotmail.com>
2025-11-25 18:31:37 -0400

    Client model created

---
+++ backend/src/models/Client.ts
@@ -0,0 +1,54 @@
+import mongoose, { Schema, Document} from "mongoose";
+import { lowercase, trim } from "zod";
+
+export interface IClient extends Document {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    address?: string;
+    taxId?: string;
+    active: boolean;
+}
+
+const ClientSchema: Schema = new Schema({
+    firstName: { 
+        type: String, 
+        required: true,
+        trim: true 
+    },  
+    lastName: { 
+        type: String, 
+        required: true,
+        trim: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        trim: true      
+    },
+    address: {
+        type: String,
+        trim: true
+    },
+    taxId: {
+        type: String,
+        trim: true
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true
+});
+
+const Client = mongoose.model<IClient>('Client', ClientSchema);
+export default Client;
